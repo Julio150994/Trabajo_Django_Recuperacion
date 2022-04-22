3b70c665efaf1dddf9bb8580b0b4f8da37b377a5
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-04-22 12:34:21 +0200

    Cambios en los modelos

--- seguroprivado/models.py
+++ seguroprivado/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Medicos(models.Model):
+class Medico(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30, null=False, blank=False, verbose_name="Nombre") 
     apellidos = models.CharField(max_length=50, null=False, blank=False, verbose_name="Apellidos")
     edad = models.IntegerField(null=False, blank=False, verbose_name="Edad")
     fechaalta = models.DateField(null=False, blank=False, verbose_name="Fecha de alta")
-    especialidad = models.CharField(max_length=40, null=False, blank=False, verbose_name="Especialidad")
+    
+    especialidades = (
+        (1,'familia'),
+        (2,'digestivo'),
+        (3,'neurólogo'),
+        (4,'dermatólogo'),
+        (5,'traumatólogo'),
+    )    
+    especialidad = models.CharField(max_length=40, null=False, blank=False, choices=especialidades, verbose_name="Especialidad")
     username = models.CharField(max_length=30, unique=True, null=False, blank=False, verbose_name="Nombre de usuario")
     password = models.CharField(max_length=30, null=False, blank=False, verbose_name="Contraseña")
 
@@ -20,11 +28,17 @@ class Medicos(models.Model):
     def __str__(self):
         return self.nombre+" "+self.apellidos+" "+str(self.edad)+" "+str(self.fechaalta)+" "+self.especialidad+" "+self.username+" "+self.password
 
-class Medicamentos(models.Model):
+class Medicamento(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50, null=False, blank=False, verbose_name="Nombre")
     descripcion = models.CharField(max_length=100, null=False, blank=False, verbose_name="Descripción")
-    receta = models.CharField(max_length=1, null=False, blank=False, verbose_name="Receta")
+    
+    recetas = (
+        (1,'s'),
+        (2,'n'),
+    )
+    
+    receta = models.CharField(max_length=1, null=False, blank=False, choices=recetas, verbose_name="Receta")
     precio = models.FloatField(null=False, blank=False, verbose_name="Precio")
     stock = models.IntegerField(null=False, blank=False, verbose_name="Stock")
     
@@ -36,7 +50,7 @@ class Medicamentos(models.Model):
     def __str__(self):
         return self.nombre+" "+self.descripcion+" "+self.receta+" "+str(self.precio)+" "+str(self.stock)
 
-class Pacientes(models.Model):
+class Paciente(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30, null=False, blank=False, verbose_name="Nombre") 
     apellidos = models.CharField(max_length=50, null=False, blank=False, verbose_name="Apellidos")
@@ -54,10 +68,10 @@ class Pacientes(models.Model):
     def __str__(self):
         return self.nombre+" "+self.apellidos+" "+str(self.edad)+" "+self.direccion+" "+str(self.foto)+" "+self.username+" "+self.password
 
-class Citas(models.Model):
+class Cita(models.Model):
     id = models.AutoField(primary_key=True)
-    idPaciente = models.ForeignKey(Pacientes, verbose_name="Paciente", on_delete=models.CASCADE)
-    idMedico = models.ForeignKey(Medicos, verbose_name="Médico", on_delete=models.CASCADE)
+    idPaciente = models.ForeignKey(Paciente, verbose_name="Paciente", on_delete=models.CASCADE)
+    idMedico = models.ForeignKey(Medico, verbose_name="Médico", on_delete=models.CASCADE)
     fecha = models.DateField(null=False, blank=False, verbose_name="Fecha")
     observaciones = models.TextField(null=False, blank=False, verbose_name="Observaciones")
     
@@ -70,11 +84,11 @@ class Citas(models.Model):
         return str(self.idPaciente)+" "+str(self.idMedico)+" "+str(self.fecha)+" "+self.observaciones
 
 
-class Compras(models.Model):
+class Compra(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField(null=False, blank=False, verbose_name="Fecha")
     precio = models.FloatField(null=False, blank=False, verbose_name="Precio")
-    idPaciente = models.ForeignKey(Pacientes, verbose_name="Paciente", on_delete=models.CASCADE)
+    idPaciente = models.ForeignKey(Paciente, verbose_name="Paciente", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name="compra"
@@ -85,9 +99,9 @@ class Compras(models.Model):
         return str(self.fecha)+" "+str(self.precio)+" "+str(self.idPaciente)
 
 
-class ComprasMedicamentos(models.Model):
-    idMedicamento = models.ForeignKey(Medicamentos, verbose_name="Medicamento", on_delete=models.CASCADE)
-    idCompra = models.ForeignKey(Compras, verbose_name="Compra", on_delete=models.CASCADE)
+class CompraMedicamento(models.Model):
+    idMedicamento = models.ForeignKey(Medicamento, verbose_name="Medicamento", on_delete=models.CASCADE)
+    idCompra = models.ForeignKey(Compra, verbose_name="Compra", on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.idMedicamento)+" "+str(self.idCompra)
