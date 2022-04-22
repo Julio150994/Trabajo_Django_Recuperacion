8e1f3490df0eaae6bdaf3df2d38727a07f301f48
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-04-22 16:07:00 +0200

    Visualización del diseño web

--- seguroprivado/views.py
+++ seguroprivado/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
 
 # Create your views here.
 
 class RedirectToInicioView(TemplateView):    
     def get(self, request):
         return HttpResponseRedirect('inicio/')
-    
 
 class TemplateInicioView(TemplateView):
     template_name = "seguroprivado/inicio.html"
