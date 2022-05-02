c02177779c2734dc646d97efa8a960374beaa97b
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-05-02 18:33:34 +0200

    Panel de admin para los pacientes

--- seguroprivado/admin.py
+++ seguroprivado/admin.py
@@ -3,9 +3,9 @@ from seguroprivado.models import Paciente
 
 class PacienteAdmin(admin.ModelAdmin):
     list_display = ("nombre","apellidos","edad","direccion","foto","username","password")
-    search_fields = ("nombre","username")
-    list_filter = ("username")
-    ordering = ("-id")
+    search_fields = ("nombre","username",)
+    list_filter = ("username",)
+    ordering = ("-id",)
 
 
 # Register your models here.
