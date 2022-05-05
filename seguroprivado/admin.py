11d8cc792838f78d341db2a4f37546006946ec04
Julio150994 <72436674+Julio150994@users.noreply.github.com>
2022-05-05 09:00:05 +0200

    Cambio en fichero admin.py

--- seguroprivado/admin.py
+++ seguroprivado/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from seguroprivado.models import Paciente
+from seguroprivado.models import Paciente, Medico
 
 class PacienteAdmin(admin.ModelAdmin):
     list_display = ("nombre","apellidos","edad","direccion","foto","activo","username","password")
@@ -7,7 +7,7 @@ class PacienteAdmin(admin.ModelAdmin):
     list_filter = ("username",)
     ordering = ("-id",)
 
-
 # Register your models here.
 
 admin.site.register(Paciente)
+admin.site.register(Medico)
