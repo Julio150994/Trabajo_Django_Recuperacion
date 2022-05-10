d2ec858dccd3b0c0df22ccdedd8a9f4e9405aff4
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-05-11 01:28:11 +0200

    Cambios en los decoradores

--- seguroprivado/decorators.py
+++ seguroprivado/decorators.py
@@ -1,21 +1,21 @@
 from django.contrib.auth.decorators import user_passes_test
 
 
-def RolAdmin(function=None, login='sign_in'):
+def RolAdmin(function=None, login='login'):
     decorador = user_passes_test(lambda usuario: usuario.is_active
         and usuario.is_superuser, login_url=login,)
     
     if function:
         return decorador
 
-def RolMedico(function=None, login='sign_in'):
+def RolMedico(function=None, login='login'):
     decorador = user_passes_test(lambda usuario: usuario.is_active
-        and usuario.is_staff and not usuario.is_superuser, login_url=login,)
+        and not usuario.is_superuser and usuario.is_staff, login_url=login,)
     
     if function:
         return decorador
 
-def RolPaciente(function=None, login='sign_in'):
+def RolPaciente(function=None, login='login'):
     decorador = user_passes_test(lambda usuario: usuario.is_active
         and not usuario.is_superuser and not usuario.is_staff, login_url=login,)
     
