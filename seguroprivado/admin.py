b424be40339a8d27b8cfe8f570af1dd3a26a12e4
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-05-11 11:11:09 +0200

    Cambios en panel de admin

--- seguroprivado/admin.py
+++ seguroprivado/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from seguroprivado.models import Paciente, Medico
 
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ["nombre","apellidos","edad","fechaalta","especialidad","username","password"]
+    ordering = ["-id"]
+    
 class PacienteAdmin(admin.ModelAdmin):
-    list_display = ("nombre","apellidos","edad","direccion","foto","activo","username","password")
-    search_fields = ("nombre","username",)
-    list_filter = ("username",)
-    ordering = ("-id",)
+    list_display = ["nombre","apellidos","edad","direccion","foto","activo","username","password"]
+    ordering = ["-id"]
 
 # Register your models here.
 
-admin.site.register(Paciente)
-admin.site.register(Medico)
+admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Paciente, PacienteAdmin)
