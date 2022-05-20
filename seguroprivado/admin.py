3d0c110a593d6bebccd367f9cfc1caee87016ce4
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-05-20 13:28:56 +0200

    Cambios en las configuraciones del panel
    Correcciones en las validaciones

--- seguroprivado/admin.py
+++ seguroprivado/admin.py
@@ -8,174 +8,124 @@ class MedicoAdminForm(forms.ModelForm):
     def clean_nombre(self):
         nombre = self.cleaned_data['nombre']
         
-        if nombre is None:
-            raise forms.ValidationError('Debe introducir un nombre para el médico')
+        if len(nombre) > 30:
+            raise forms.ValidationError('El nombre del médico debe tener 30 caracteres como máximo')
         else:
-            if len(nombre) > 30:
-                raise forms.ValidationError('El nombre del médico debe tener 30 caracteres como máximo')
-            else:
-                return nombre
+            return nombre
         
     def clean_apellidos(self):
         apellidos = self.cleaned_data['apellidos']
         
-        if apellidos is None:
-            raise forms.ValidationError('Debe introducir los apellidos para el médico')
+        if len(apellidos) > 50:
+            raise forms.ValidationError('Los apellidos deben comprender 50 caracteres como máximo')
         else:
-            if len(apellidos) > 50:
-                raise forms.ValidationError('Los apellidos deben comprender 50 caracteres como máximo')
-            else:
-                return apellidos
+            return apellidos
         
     def clean_edad(self):
         edad = self.cleaned_data['edad']
         
-        if edad is None:
-            raise forms.ValidationError('Debe introducir una edad para el médico')
+        if edad < 0:
+            raise forms.ValidationError('La edad '+str(edad)+" no es válida")
         else:
-            if edad < 0:
-                raise forms.ValidationError('La edad '+str(edad)+" no es válida")
-            else:
-                return edad
+            return edad
         
     def clean_fechaalta(self):
         fechaalta = self.cleaned_data['fechaalta']
         
-        if fechaalta is None:
-            raise forms.ValidationError('Debe introducir una fecha de alta para el médico')
-        else:
-            set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
-            set_fecha_alta = datetime.strptime(fechaalta,'%Y-%m-%d')
-            
-            if set_fecha_alta <= set_fecha_actual:
-                raise forms.ValidationError('La fecha de alta no debe ser menor o igual que la fecha actual')
-            else:
-                return fechaalta
+        set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
+        set_fecha_alta = datetime.strptime(fechaalta,'%Y-%m-%d')
         
-    def clean_especialidad(self):
-        especialidad = self.cleaned_data['especialidad']
-        
-        if especialidad is None:
-            raise forms.ValidationError('Debe seleccionar una especialidad')
+        if set_fecha_alta <= set_fecha_actual:
+            raise forms.ValidationError('La fecha de alta no debe ser menor o igual que la fecha actual')
         else:
-            return especialidad
+            return fechaalta
     
     def clean_username(self):
         username = self.cleaned_data['username']
         
-        if username is None:
-            raise forms.ValidationError('Debe introducir un nombre de usuario para el médico')
+        if Medico.objects.filter(username=username).exists():
+            raise forms.ValidationError('El nombre de usuario médico '+str(username)+' ya existe')
         else:
-            if Medico.objects.filter(username=username).exists():
-                raise forms.ValidationError('El nombre de usuario médico '+str(username)+' ya existe')
+            if len(username) > 30:
+                raise forms.ValidationError('El nombre de usuario debe tener 30 caracteres como máximo')
             else:
-                if len(username) > 30:
-                    raise forms.ValidationError('El nombre de usuario debe tener 30 caracteres como máximo')
-                else:
-                    return username
+                return username
         
     def clean_password(self):
         password = self.cleaned_data['password']
         
-        if password is None:
-            raise forms.ValidationError('Debe introducir una contraseña para el médico')
+        if len(password) > 30:
+            raise forms.ValidationError('La contraseña del médico debe tener 30 caracteres como máximo')
         else:
-            if len(password) > 30:
-                raise forms.ValidationError('La contraseña del médico debe tener 30 caracteres como máximo')
-            else:
-                return password
+            return password
 
 class MedicamentoAdminForm(forms.ModelForm):
     def clean_nombre(self):
         nombre = self.cleaned_data['nombre']
         
-        if nombre is None:
-            raise forms.ValidationError('Debe introducir un nombre para el medicamento')
+        if Medicamento.objects.filter(nombre=nombre).exists():
+            raise forms.ValidationError('El nombre de medicamento '+str(nombre)+' ya existe.')
         else:
-            if Medicamento.objects.filter(nombre=nombre).exists():
-                raise forms.ValidationError('El nombre de medicamento '+str(nombre)+' ya existe.')
+            if len(nombre) > 50:
+                raise forms.ValidationError('El nombre '+str(nombre)+' debe tener 50 caracteres como máximo')
             else:
-                if len(nombre) > 50:
-                    raise forms.ValidationError('El nombre '+str(nombre)+' debe tener 50 caracteres como máximo')
-                else:
-                    return nombre
+                return nombre
         
     def clean_descripcion(self):
         descripcion = self.cleaned_data['descripcion']
 
-        if descripcion is None:
-            raise forms.ValidationError('Debe introducir una descripción para el medicamento')
+        if len(descripcion) > 100:
+            raise forms.ValidationError('La descripción debe tener 100 caracteres como máximo')
         else:
-            if len(descripcion) > 100:
-                raise forms.ValidationError('La descripción debe tener 100 caracteres como máximo')
-            else:
-                return descripcion
-        
-    def clean_receta(self):
-        receta = self.cleaned_data['receta']
-        
-        if receta is None:
-            raise forms.ValidationError('Debe seleccionar una receta')
-        else:
-            return receta
-        
+            return descripcion
+    
     def clean_precio(self):
         precio = self.cleaned_data['precio']
         
-        if precio is None:
-            raise forms.ValidationError('Debe introducir un precio')
+        get_decimales = str(precio)[str(precio).find(".")+1:]
+    
+        if len(get_decimales) != 2:
+            raise forms.ValidationError('El número de precio debe contener dos decimales')
         else:
-            get_decimales = str(precio)[str(precio).find(".")+1:]
-        
-            if len(get_decimales) != 2:
-                raise forms.ValidationError('El número de precio debe contener dos decimales')
-            else:
-                return precio
+            return precio
 
 
 class PacienteAdminForm(forms.ModelForm):
     def clean_nombre(self):
         nombre = self.cleaned_data['nombre']
-        if nombre is None:
-            raise forms.ValidationError('Debe introducir un nombre para el paciente')
-        else:            
-            if len(nombre) > 30:
-                raise forms.ValidationError('El nombre del médico debe tener 30 caracteres como máximo')
-            else:
-                return nombre
+                    
+        if len(nombre) > 30:
+            raise forms.ValidationError('El nombre del paciente debe tener 30 caracteres como máximo')
+        else:
+            return nombre
         
     def clean_apellidos(self):
         apellidos = self.cleaned_data['apellidos']
-        if apellidos is None:
-            raise forms.ValidationError('Debe introducir los apellidos para el paciente')
+        
+        if len(apellidos) > 50:
+            raise forms.ValidationError('Los apellidos deben comprender 50 caracteres como máximo')
         else:
-            if len(apellidos) > 50:
-                raise forms.ValidationError('Los apellidos deben comprender 50 caracteres como máximo')
-            else:
-                return apellidos
+            return apellidos
         
     def clean_edad(self):
         edad = self.cleaned_data['edad']
-        if edad is None:
-            raise forms.ValidationError('Debe introducir una edad para el paciente')
+        
+        if edad < 0:
+            raise forms.ValidationError('La edad para el paciente no es válida')
         else:
-            if edad < 0:
-                raise forms.ValidationError('La edad para el paciente no es válida')
-            else:
-                return edad
+            return edad
         
     def clean_direccion(self):
         direccion = self.cleaned_data['direccion']
-        if direccion is None:
-            raise forms.ValidationError('Debe escribir una dirección para el paciente')
+        
+        if len(direccion) > 100:
+            raise forms.ValidationError('La dirección debe tener 100 caracteres como máximo')
         else:
-            if len(direccion) > 100:
-                raise forms.ValidationError('La dirección debe tener 100 caracteres como máximo')
-            else:
-                return direccion
+            return direccion
         
     def clean_foto(self):
         foto = self.cleaned_data['foto']
+        
         if foto is None:
             raise forms.ValidationError('Debe seleccionar una foto para el paciente')
         else:
@@ -183,24 +133,20 @@ class PacienteAdminForm(forms.ModelForm):
     
     def clean_username(self):
         username = self.cleaned_data['username']
-        if username is None:
-            raise forms.ValidationError('Debe introducir un nombre de usuario para el paciente')
-        else:
-            if Paciente.objects.filter(username=username).exists():    
-                if len(username) > 30:
-                    raise forms.ValidationError('El nombre de usuario debe tener 30 caracteres como máximo')
-                else:
-                    return username
+        
+        if Paciente.objects.filter(username=username).exists():    
+            if len(username) > 30:
+                raise forms.ValidationError('El nombre de usuario debe tener 30 caracteres como máximo')
+            else:
+                return username
         
     def clean_password(self):
         password = self.cleaned_data['password']
-        if password is None:
-            raise forms.ValidationError('Debe introducir una contraseña para el paciente')
+        
+        if len(password) > 30:
+            raise forms.ValidationError('La contraseña del paciente debe tener 30 caracteres como máximo')
         else:
-            if len(password) > 30:
-                raise forms.ValidationError('La contraseña del paciente debe tener 30 caracteres como máximo')
-            else:
-                return password
+            return password
 
 class CitaAdminForm(forms.ModelForm):
     def clean_idPaciente(self):
@@ -222,54 +168,43 @@ class CitaAdminForm(forms.ModelForm):
     def clean_fecha(self):
         fecha = self.cleaned_data['fecha']
         
-        if fecha is None:
-            raise forms.ValidationError('Debe introducir una fecha de cita para el paciente')
+        set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
+        set_fecha_cita = datetime.strptime(fecha,'%Y-%m-%d')
+        
+        if set_fecha_cita <= set_fecha_actual:
+            raise forms.ValidationError('La fecha de la cita no debe ser menor o igual que la fecha actual')
         else:
-            set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
-            set_fecha_cita = datetime.strptime(fecha,'%Y-%m-%d')
-            
-            if set_fecha_cita <= set_fecha_actual:
-                raise forms.ValidationError('La fecha de la cita no debe ser menor o igual que la fecha actual')
-            else:
-                return fecha
+            return fecha
     
     def clean_observaciones(self):
         observaciones = self.cleaned_data['observaciones']
-        if observaciones is None:
-            raise forms.ValidationError('Debe introducir un nombre de usuario para el médico')
+        
+        if len(observaciones) > 100:
+            raise forms.ValidationError('Las observaciones debe contener 100 caracteres como máximo')
         else:
-            if len(observaciones) > 100:
-                raise forms.ValidationError('Las observaciones debe contener 100 caracteres como máximo')
-            else:
-                return observaciones
+            return observaciones
 
 class CompraAdminForm(forms.ModelForm):
     def clean_fecha(self):
         fecha_compra = self.cleaned_data['fecha']
 
-        if fecha_compra is None:
-            raise forms.ValidationError('Debe introducir una fecha de compra del paciente')
+        set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
+        set_fecha_compra = datetime.strptime(fecha_compra,'%Y-%m-%d')
+        
+        if set_fecha_compra > set_fecha_actual:
+            raise forms.ValidationError('La fecha de compra no debe ser mayor que la fecha actual')
         else:
-            set_fecha_actual = datetime(int(datetime.now().year),int(datetime.now().month),int(datetime.now().day))
-            set_fecha_compra = datetime.strptime(fecha_compra,'%Y-%m-%d')
-            
-            if set_fecha_compra > set_fecha_actual:
-                raise forms.ValidationError('La fecha de compra no debe ser mayor que la fecha actual')
-            else:
-                return fecha_compra
+            return fecha_compra
             
     def clean_precio(self):
         precio = self.cleaned_data['precio']
         
-        if precio is None:
-            raise forms.ValidationError('Debe introducir un precio de compra')
+        get_decimales = str(precio)[str(precio).find(".")+1:]
+    
+        if len(get_decimales) != 2:
+            return forms.ValidationError('El precio de la compra debe contener dos decimales')
         else:
-            get_decimales = str(precio)[str(precio).find(".")+1:]
-        
-            if len(get_decimales) != 2:
-                return forms.ValidationError('El precio de la compra debe contener dos decimales')
-            else:
-                return precio
+            return precio
     
     def clean_idPaciente(self):
         id_paciente = self.cleaned_data['idPaciente']
